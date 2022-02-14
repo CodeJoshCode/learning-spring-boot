59cfbd18170df3b6ab1e8d49cebc799573171a74
Josh Simmons <simmonsj711@dupage.edu>
2022-02-14 09:23:58 -0600

    comments for hello world rest controller class

--- src/main/java/com/airblox/learningspringboot/HelloController.java
+++ src/main/java/com/airblox/learningspringboot/HelloController.java
@@ -3,9 +3,16 @@ package com.airblox.learningspringboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+Author: Josh Simmons
+Date: 2/14/22
+Main controller class from https://spring.io/guides/gs/spring-boot/
+for a simple hello world application.
+*/
 @RestController
 public class HelloController {
 
+    // mapping at / to return a thymeleaf page with hello world text in the body
     @GetMapping("/")
     public String index(){
         return "Greetings from Josh in Spring Boot!";
