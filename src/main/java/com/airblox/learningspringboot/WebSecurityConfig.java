cab271eb7567b4b5cf0db3aa1a7d247874dc0f9e
Joshua Simmons <joshuasimmons@Joshuas-MacBook-Pro.local>
2022-02-15 09:58:07 -0600

    Basic Spring Security

--- src/main/java/com/airblox/learningspringboot/WebSecurityConfig.java
+++ src/main/java/com/airblox/learningspringboot/WebSecurityConfig.java
@@ -1,3 +1,5 @@
+package com.airblox.learningspringboot;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,7 +9,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+
+/*
+    
+*/
 
 @Configuration
 @EnableWebSecurity
