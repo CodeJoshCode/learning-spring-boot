fc7ac4ea834cba3a2a8fd77eaa9b65ca1e4eb79f
Joshua Simmons <joshuasimmons@Joshuas-MacBook-Pro.local>
2022-03-01 08:47:36 -0600

    moved business logic for product service to a service class

--- src/main/java/com/airblox/learningspringboot/controller/ProductServiceController.java
+++ src/main/java/com/airblox/learningspringboot/controller/ProductServiceController.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.airblox.learningspringboot.model.Product;
+import com.airblox.learningspringboot.service.ProductService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,46 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductServiceController{
-    private static Map<String, Product> productRepo = new HashMap<>();
-    static{
-        Product honey = new Product();
-        honey.setId("1");
-        honey.setName("honey");
-        productRepo.put(honey.getId(), honey);
-
-        Product almond = new Product();
-        almond.setId("2");
-        almond.setName("almond");
-        productRepo.put(almond.getId(), almond);
-    }
+    @Autowired
+    ProductService productService;
 
     @RequestMapping(value = "/products", method=RequestMethod.POST)
     public ResponseEntity<Object> createProduct(@RequestBody Product product){
-        productRepo.put(product.getId(), product);
+        productService.createProduct(product);
         return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED); 
     }
 
     @RequestMapping(value = "/products/{id}", method=RequestMethod.PUT)
     public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) throws ProductNotFoundException{
-        if(!productRepo.containsKey(id)){
-            throw new ProductNotFoundException();
-        }
-        productRepo.remove(id);
-        product.setId(id);
-        productRepo.put(id, product);
+        productService.updateProduct(id, product);
         return new ResponseEntity<>("Product is updated successfully", HttpStatus.OK);
     }
 
     @RequestMapping(value="/products")
     public ResponseEntity<Object> getProduct(
         @RequestParam(value = "name", required=false, defaultValue="honey") String name){
-            return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+            return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
     }
 
     @RequestMapping(value="/products/{id}", method=RequestMethod.DELETE)
     public ResponseEntity<Object> delete(@PathVariable("id") String id)
     {
-        productRepo.remove(id);
+        productService.deleteProduct(id);
         return new ResponseEntity<>("Successfully deleted Product", HttpStatus.OK);
     }
 
