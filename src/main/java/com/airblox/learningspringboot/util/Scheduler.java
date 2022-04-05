ca69c4320718739eac33da1e2858908b63725805
Joshua <joshua@airblox.com>
2022-04-05 12:04:51 -0400

    added fixed rate cron job

--- src/main/java/com/airblox/learningspringboot/util/Scheduler.java
+++ src/main/java/com/airblox/learningspringboot/util/Scheduler.java
@@ -9,10 +9,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class Scheduler {
+    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @Scheduled(cron = "0 * 9 * * ?")
     public void cronJob(){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         Date now = new Date();
         String strDate = simpleDateFormat.format(now);
         log.info("Java cron expression::" + strDate);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void fixedRateCronJob(){
+        Date now = new Date();
+        String strDate = simpleDateFormat.format(now);
+        log.info("Fixed rate cron job::" + strDate);
     }
 }
